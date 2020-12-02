295de9d16089893406fd76ba8290b1478ebba0ae
Marc Loehe <loehe@sipgate.de>
2020-12-02 11:43:36 +0100

    add first attempt on day2 part2

--- src/main/java/us/byteb/advent/twenty/Day2.java
+++ src/main/java/us/byteb/advent/twenty/Day2.java
@@ -13,20 +13,31 @@ import java.util.stream.Collectors;
 public class Day2 {
 
   public static void main(String[] args) throws Exception {
-    System.out.println(partOne());
-  }
-
-  private static Map<String, Integer> partOne() throws IOException {
     final List<String> lines = readFileFromResources("day2/input.txt").lines().collect(Collectors.toList());
 
+    System.out.println("part1: " + partOne(lines));
+    System.out.println("part2: " + partTwo(lines));
+  }
+
+  private static Map<String, Integer> partOne(final List<String> lines) throws IOException {
     final Map<String, Integer> result = lines.parallelStream()
         .map(ValidationItem::parse)
-        .map(ValidationItem::test)
+        .map(ValidationItem::testFirstPolicy)
         .collect(Collectors.groupingBy(result1 -> result1 ? "valid" : "invalid")).entrySet().stream().collect(Collectors.toMap(
             Map.Entry::getKey,
             entry -> entry.getValue().size()
         ));
     return result;
+  }
+
+  private static Map<String, Integer> partTwo(final List<String> lines) throws IOException {
+    return lines.parallelStream()
+        .map(ValidationItem::parse)
+        .map(ValidationItem::testSecondPolicy)
+        .collect(Collectors.groupingBy(result1 -> result1 ? "valid" : "invalid")).entrySet().stream().collect(Collectors.toMap(
+            Map.Entry::getKey,
+            entry -> entry.getValue().size()
+        ));
   }
 
 
@@ -43,11 +54,11 @@ public class Day2 {
   private record ValidationRule(Range range, char requiredChar) {
   }
 
-  private record ValidationItem(ValidationRule rule, String password) {
+  record ValidationItem(ValidationRule rule, String password) {
 
     private static final Pattern PATTERN = Pattern.compile("(\\d+)-(\\d+)\\s+(\\w):\\s+(\\w+)");
 
-    private static ValidationItem parse(final String line) {
+    static ValidationItem parse(final String line) {
       final List<String> matches = findAllMatchingGroups(line, PATTERN);
       if (matches.size() != 4) {
         throw new IllegalStateException("Unable to parse input line: " + line);
@@ -71,9 +82,15 @@ public class Day2 {
     }
 
 
-    public final boolean test() {
+    public final boolean testFirstPolicy() {
       final long numOccurrences = password.chars().filter(c -> c == rule.requiredChar).count();
       return numOccurrences >= rule.range().lower() && numOccurrences <= rule.range().upper();
+    }
+
+
+    public final boolean testSecondPolicy() {
+      return password.charAt(rule.range.lower() - 1) == rule.requiredChar
+          && password.charAt(rule.range.upper() - 1) != rule.requiredChar;
     }
 
   }
---
+++ src/test/java/us/byteb/advent/twenty/Day2Test.java
@@ -0,0 +1,34 @@
+package us.byteb.advent.twenty;
+
+import org.junit.jupiter.api.extension.ExtensionContext;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.ArgumentsProvider;
+import org.junit.jupiter.params.provider.ArgumentsSource;
+import us.byteb.advent.twenty.Day2.ValidationItem;
+
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class Day2Test {
+
+  @ParameterizedTest
+  @ArgumentsSource(InputProvider.class)
+  void testPartTwoExamples(final String input, boolean expected) {
+    assertEquals(expected, ValidationItem.parse(input).testSecondPolicy());
+  }
+
+  static class InputProvider implements ArgumentsProvider {
+
+    @Override
+    public Stream<? extends Arguments> provideArguments(ExtensionContext context) {
+      return Stream.of(
+          Arguments.of("1-3 a: abcde", true),
+          Arguments.of("1-3 b: cdefg", false),
+          Arguments.of("2-9 c: ccccccccc", false)
+      );
+    }
+  }
+
+}
