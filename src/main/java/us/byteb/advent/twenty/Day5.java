bcac7a578461558a5ae64765c37134dad5bdfb4c
Marc Löhe <marc@marcloehe.de>
2020-12-05 14:21:43 +0100

    feat: add day5 part2

--- src/main/java/us/byteb/advent/twenty/Day5.java
+++ src/main/java/us/byteb/advent/twenty/Day5.java
@@ -1,15 +1,31 @@
 package us.byteb.advent.twenty;
 
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
 public class Day5 {
 
   public static void main(String[] args) {
     final String input = Utils.readFileFromResources("day5/input.txt");
 
-    System.out.println(
-        "Part 1: " + input.lines().map(Seat::of).mapToLong(Seat::calcSeatId).max().getAsLong());
+    final List<Seat> seats = input.lines().map(Seat::of).sorted().collect(Collectors.toList());
+
+    System.out.println("Part 1: " + seats.stream().mapToInt(Seat::calcSeatId).max().getAsInt());
+
+    final List<Integer> seatIds =
+        seats.stream().map(seat -> seat.calcSeatId()).collect(Collectors.toList());
+    final int minSeatId = seatIds.stream().mapToInt(id -> id).min().getAsInt();
+    final int maxSeatId = seatIds.stream().mapToInt(id -> id).max().getAsInt();
+    final List<Integer> missingSeatIds =
+        IntStream.range(minSeatId, maxSeatId)
+            .filter(seatId -> !seatIds.contains(seatId))
+            .boxed()
+            .collect(Collectors.toList());
+    System.out.println("Part 2: " + missingSeatIds);
   }
 
-  record Seat(long row, long column) {
+  record Seat(int row, int column) implements Comparable<Seat> {
     public static Seat of(final String code) {
       return new Seat(parse(code.substring(0, 7), "B", "F"), parse(code.substring(7), "R", "L"));
     }
@@ -18,8 +34,17 @@ public class Day5 {
       return Integer.parseInt(input.replaceAll(oneChar, "1").replaceAll(zeroChar, "0"), 2);
     }
 
-    public long calcSeatId() {
+    public int calcSeatId() {
       return (row * 8) + column;
+    }
+
+    @Override
+    public int compareTo(final Seat other) {
+      final int rowCompare = this.row() - other.row;
+      if (rowCompare != 0) {
+        return rowCompare;
+      }
+      return this.column() - other.column();
     }
   }
 }
