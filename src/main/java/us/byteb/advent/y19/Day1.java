aa10cfbafb826751ce9fb20a5e1d3892846cde3b
Marc Löhe <marc@marcloehe.de>
2020-12-05 19:44:29 +0100

    feat: add y19 day1 part2

--- src/main/java/us/byteb/advent/y19/Day1.java
+++ src/main/java/us/byteb/advent/y19/Day1.java
@@ -9,9 +9,22 @@ public class Day1 {
 
     System.out.println(
         "Part 1: " + input.lines().mapToLong(line -> calcRequiredFuel(Long.parseLong(line))).sum());
+
+    System.out.println(
+        "Part 2: "
+            + input
+                .lines()
+                .mapToLong(line -> calcRequiredFuelWithFuelForFuel(Long.parseLong(line)))
+                .sum());
   }
 
   static long calcRequiredFuel(long moduleMass) {
     return (moduleMass / 3) - 2;
+  }
+
+  static long calcRequiredFuelWithFuelForFuel(long moduleMass) {
+    final long fuel = (moduleMass / 3) - 2;
+
+    return fuel <= 3 ? Math.max(fuel, 0) : fuel + calcRequiredFuelWithFuelForFuel(fuel);
   }
 }
--- src/test/java/us/byteb/advent/y19/Day1Test.java
+++ src/test/java/us/byteb/advent/y19/Day1Test.java
@@ -2,6 +2,7 @@ package us.byteb.advent.y19;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static us.byteb.advent.y19.Day1.calcRequiredFuel;
+import static us.byteb.advent.y19.Day1.calcRequiredFuelWithFuelForFuel;
 
 import org.junit.jupiter.api.Test;
 
@@ -13,5 +14,12 @@ class Day1Test {
     assertEquals(2, calcRequiredFuel(14));
     assertEquals(654, calcRequiredFuel(1969));
     assertEquals(33583, calcRequiredFuel(100756));
+  }
+
+  @Test
+  void testCalcRequiredFuelWithFuelForFuel() {
+    assertEquals(2, calcRequiredFuelWithFuelForFuel(12));
+    assertEquals(966, calcRequiredFuelWithFuelForFuel(1969));
+    assertEquals(50346, calcRequiredFuelWithFuelForFuel(100756));
   }
 }
