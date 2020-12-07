6089f1fb2112ad7e34ebfcc0f366ec203a7f9f27
Marc Löhe <marc@marcloehe.de>
2020-12-07 23:12:59 +0100

    refactor: fix formatting

--- src/main/java/us/byteb/advent/y20/Day7.java
+++ src/main/java/us/byteb/advent/y20/Day7.java
@@ -1,43 +1,47 @@
 package us.byteb.advent.y20;
 
-import org.apache.commons.lang3.tuple.Pair;
-import us.byteb.advent.Utils;
-
 import java.util.*;
 import java.util.stream.Collectors;
+import org.apache.commons.lang3.tuple.Pair;
+import us.byteb.advent.Utils;
 
 public class Day7 {
 
   public static void main(String[] args) {
-    final Map<String, Map<String, Integer>> rules = parseRules(Utils.readFileFromResources("y20/day7.txt"));
-    System.out.println("Part 1: " + findAllPossibleContainingBagColor("shiny gold", rules).size());;
+    final Map<String, Map<String, Integer>> rules =
+        parseRules(Utils.readFileFromResources("y20/day7.txt"));
+    System.out.println("Part 1: " + findAllPossibleContainingBagColor("shiny gold", rules).size());
+    ;
     System.out.println("Part 2: " + findNumBags("shiny gold", rules));
-
   }
 
   static Map<String, Map<String, Integer>> parseRules(final String input) {
-    return input.lines()
-        .map(line -> {
-          final String[] parts = line.split(" bags contain ");
-          return Pair.of(parts[0], parts[1]);
-        })
-        .collect(Collectors.toMap(
-            Pair::getLeft,
-        line -> {
-          final String definition = line.getRight().replaceAll("\\.", "");
-          if (definition.equals("no other bags")) {
-            return Collections.emptyMap();
-          }
-          return Arrays.stream(definition
-              .split(",")).map(String::strip)
-              .collect(Collectors.toMap(
-                  str -> str.replaceAll("\\d+", "").replaceAll("bags?", "").strip(),
-                  str -> Integer.parseInt(str.replaceAll("\\D+", ""))
-              ));
-        }));
+    return input
+        .lines()
+        .map(
+            line -> {
+              final String[] parts = line.split(" bags contain ");
+              return Pair.of(parts[0], parts[1]);
+            })
+        .collect(
+            Collectors.toMap(
+                Pair::getLeft,
+                line -> {
+                  final String definition = line.getRight().replaceAll("\\.", "");
+                  if (definition.equals("no other bags")) {
+                    return Collections.emptyMap();
+                  }
+                  return Arrays.stream(definition.split(","))
+                      .map(String::strip)
+                      .collect(
+                          Collectors.toMap(
+                              str -> str.replaceAll("\\d+", "").replaceAll("bags?", "").strip(),
+                              str -> Integer.parseInt(str.replaceAll("\\D+", ""))));
+                }));
   }
 
-  static Set<String> findAllPossibleContainingBagColor(final String bagColor, final Map<String, Map<String, Integer>> rules) {
+  static Set<String> findAllPossibleContainingBagColor(
+      final String bagColor, final Map<String, Map<String, Integer>> rules) {
     final HashSet<String> consideredColors = new HashSet<>();
     consideredColors.add(bagColor);
 
@@ -47,29 +51,36 @@ public class Day7 {
     return colors;
   }
 
-  private static Set<String> findAllPossibleContainingBagColor(final String bagColor, final Set<String> consideredColors, final Map<String, Map<String, Integer>> rules) {
-    final Set<String> newPossibilities = rules.entrySet().stream()
-        .filter(entry -> entry.getValue().containsKey(bagColor))
-        .map(Map.Entry::getKey)
-        .filter(c -> !consideredColors.contains(c))
-        .collect(Collectors.toSet());
+  private static Set<String> findAllPossibleContainingBagColor(
+      final String bagColor,
+      final Set<String> consideredColors,
+      final Map<String, Map<String, Integer>> rules) {
+    final Set<String> newPossibilities =
+        rules.entrySet().stream()
+            .filter(entry -> entry.getValue().containsKey(bagColor))
+            .map(Map.Entry::getKey)
+            .filter(c -> !consideredColors.contains(c))
+            .collect(Collectors.toSet());
 
     for (final String c : newPossibilities) {
       consideredColors.add(c);
 
       final Map<String, Integer> colorRules = rules.get(c);
-      if (!colorRules.isEmpty()) {findAllPossibleContainingBagColor(c, consideredColors, rules);
+      if (!colorRules.isEmpty()) {
+        findAllPossibleContainingBagColor(c, consideredColors, rules);
       }
     }
 
     return consideredColors;
   }
 
-  public static long findNumBags(final String bagColor, final Map<String, Map<String, Integer>> rules) {
+  public static long findNumBags(
+      final String bagColor, final Map<String, Map<String, Integer>> rules) {
     final Map<String, Integer> bagRules = rules.get(bagColor);
 
     return bagRules.entrySet().stream()
-        .mapToLong(entry -> entry.getValue() + (entry.getValue() * findNumBags(entry.getKey(), rules)))
+        .mapToLong(
+            entry -> entry.getValue() + (entry.getValue() * findNumBags(entry.getKey(), rules)))
         .sum();
   }
 }
--- src/test/java/us/byteb/advent/y20/Day7Test.java
+++ src/test/java/us/byteb/advent/y20/Day7Test.java
@@ -1,49 +1,48 @@
 package us.byteb.advent.y20;
 
-import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.util.Collections;
 import java.util.Map;
 import java.util.Set;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
 
 class Day7Test {
 
-  private static final Map<String, Map<String, Integer>> RULES = Map.of(
-      "light red", Map.of(
-          "bright white", 1,
-          "muted yellow", 2
-      ),
-      "dark orange", Map.of(
-          "bright white", 3,
-          "muted yellow", 4
-      ),
-      "bright white", Map.of(
-          "shiny gold", 1
-      ),
-      "muted yellow", Map.of(
-          "shiny gold", 2,
-          "faded blue", 9
-      ),
-      "shiny gold", Map.of(
-          "dark olive", 1,
-          "vibrant plum", 2
-      ),
-      "dark olive", Map.of(
-          "faded blue", 3,
-          "dotted black", 4
-      ),
-      "vibrant plum", Map.of(
-          "faded blue", 5,
-          "dotted black", 6
-      ),
-      "faded blue", Collections.emptyMap(),
-      "dotted black", Collections.emptyMap());
+  private static final Map<String, Map<String, Integer>> RULES =
+      Map.of(
+          "light red",
+              Map.of(
+                  "bright white", 1,
+                  "muted yellow", 2),
+          "dark orange",
+              Map.of(
+                  "bright white", 3,
+                  "muted yellow", 4),
+          "bright white", Map.of("shiny gold", 1),
+          "muted yellow",
+              Map.of(
+                  "shiny gold", 2,
+                  "faded blue", 9),
+          "shiny gold",
+              Map.of(
+                  "dark olive", 1,
+                  "vibrant plum", 2),
+          "dark olive",
+              Map.of(
+                  "faded blue", 3,
+                  "dotted black", 4),
+          "vibrant plum",
+              Map.of(
+                  "faded blue", 5,
+                  "dotted black", 6),
+          "faded blue", Collections.emptyMap(),
+          "dotted black", Collections.emptyMap());
 
   @Test
   void parseExample() {
-    final String input = """
+    final String input =
+        """
         light red bags contain 1 bright white bag, 2 muted yellow bags.
         dark orange bags contain 3 bright white bags, 4 muted yellow bags.
         bright white bags contain 1 shiny gold bag.
@@ -63,14 +62,21 @@ class Day7Test {
 
   @Test
   void solveExamplePart1() {
-    assertEquals(Set.of("bright white", "muted yellow", "dark orange", "light red"),Day7.findAllPossibleContainingBagColor("shiny gold", RULES));
+    assertEquals(
+        Set.of("bright white", "muted yellow", "dark orange", "light red"),
+        Day7.findAllPossibleContainingBagColor("shiny gold", RULES));
   }
 
   @Test
   void solveExamplePart2() {
     assertEquals(32, Day7.findNumBags("shiny gold", RULES));
 
-    assertEquals(126, Day7.findNumBags("shiny gold", Day7.parseRules("""
+    assertEquals(
+        126,
+        Day7.findNumBags(
+            "shiny gold",
+            Day7.parseRules(
+                """
         shiny gold bags contain 2 dark red bags.
         dark red bags contain 2 dark orange bags.
         dark orange bags contain 2 dark yellow bags.
@@ -79,6 +85,5 @@ class Day7Test {
         dark blue bags contain 2 dark violet bags.
         dark violet bags contain no other bags.
         """)));
-
   }
 }
