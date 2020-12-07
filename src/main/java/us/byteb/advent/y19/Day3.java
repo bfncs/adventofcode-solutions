3270caf6c56b7fafe08e64eaa057b300a5daeb99
Marc Löhe <marc@marcloehe.de>
2020-12-07 23:12:53 +0100

    feat: add y19 d3 p1

---
+++ src/main/java/us/byteb/advent/y19/Day3.java
@@ -0,0 +1,116 @@
+package us.byteb.advent.y19;
+
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+import us.byteb.advent.Utils;
+
+public class Day3 {
+
+  public static void main(String[] args) {
+    final List<String> input =
+        Utils.readFileFromResources("y19/day3.txt").lines().collect(Collectors.toList());
+    final WirePath path1 = WirePath.of(input.get(0));
+    final WirePath path2 = WirePath.of(input.get(1));
+
+    System.out.println(
+        "Part 1: "
+            + Point.shortestManhattanDistanceToCenter(WirePath.intersectionPoints(path1, path2))
+                .manhattanDistanceToCenter());
+  }
+
+  static String print(final WirePath path1, final WirePath path2) {
+    final Set<Point> allPoints =
+        Stream.concat(path1.points().stream(), path2.points.stream()).collect(Collectors.toSet());
+    final int xMin = allPoints.stream().mapToInt(Point::x).min().orElseThrow();
+    final int xMax = allPoints.stream().mapToInt(Point::x).max().orElseThrow();
+    final int yMin = allPoints.stream().mapToInt(Point::y).min().orElseThrow();
+    final int yMax = allPoints.stream().mapToInt(Point::y).max().orElseThrow();
+
+    final StringBuilder sb = new StringBuilder();
+    for (int y = yMax + 1; y >= yMin - 1; y--) {
+      for (int x = xMin - 1; x <= xMax + 1; x++) {
+
+        final Point currentPoint = new Point(x, y);
+        final boolean inPath1 = path1.points().contains(currentPoint);
+        final boolean inPath2 = path2.points().contains(currentPoint);
+
+        if (x == 0 && y == 0) {
+          sb.append('0');
+        } else if (inPath1 && inPath2) {
+          sb.append('X');
+        } else if (inPath1) {
+          sb.append('1');
+        } else if (inPath2) {
+          sb.append('2');
+        } else {
+          sb.append('.');
+        }
+
+        if (x > xMax) {
+          sb.append("\n");
+        }
+      }
+    }
+
+    return sb.toString();
+  }
+
+  record Point(int x, int y) {
+    long manhattanDistanceToCenter() {
+      return Math.abs(x) + Math.abs(y);
+    }
+
+    static Point shortestManhattanDistanceToCenter(final Collection<Point> points) {
+      return points.stream()
+          .collect(Collectors.toMap(p -> p, Point::manhattanDistanceToCenter))
+          .entrySet()
+          .stream()
+          .sorted((e1, e2) -> Math.toIntExact((e1.getValue() - e2.getValue())))
+          .findFirst()
+          .map(Map.Entry::getKey)
+          .orElseThrow();
+    }
+  }
+
+  record WirePath(List<Point> points) {
+    static WirePath of(final String input) {
+
+      final List<Point> points = new ArrayList<>();
+      Point currentPoint = new Point(0, 0);
+
+      for (final String instruction : input.split(",")) {
+        final char op = instruction.charAt(0);
+        final int amount = Integer.parseInt(instruction.substring(1));
+        for (int i = 0; i < amount; i++) {
+          currentPoint =
+              switch (op) {
+                case 'U':
+                  yield new Point(currentPoint.x(), currentPoint.y() + 1);
+                case 'D':
+                  yield new Point(currentPoint.x(), currentPoint.y() - 1);
+                case 'R':
+                  yield new Point(currentPoint.x() + 1, currentPoint.y());
+                case 'L':
+                  yield new Point(currentPoint.x() - 1, currentPoint.y());
+                default:
+                  throw new IllegalStateException("Illegal op: " + op);
+              };
+          points.add(currentPoint);
+        }
+      }
+
+      return new WirePath(points);
+    }
+
+    static Set<Point> intersectionPoints(final WirePath wp1, final WirePath wp2) {
+      return Stream.concat(wp1.points().stream().distinct(), wp2.points().stream().distinct())
+          .collect(Collectors.groupingBy(p -> p))
+          .entrySet()
+          .stream()
+          .filter(entry -> entry.getValue().size() > 1)
+          .map(Map.Entry::getKey)
+          .collect(Collectors.toSet());
+    }
+  }
+}
---
+++ src/test/java/us/byteb/advent/y19/Day3Test.java
@@ -0,0 +1,33 @@
+package us.byteb.advent.y19;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static us.byteb.advent.y19.Day3.*;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import us.byteb.advent.y19.Day3.WirePath;
+
+class Day3Test {
+
+  @Test
+  void testDistance() {
+    assertEquals(159, new Point(155, 4).manhattanDistanceToCenter());
+  }
+
+  @ParameterizedTest
+  @CsvSource({
+    "'R8,U5,L5,D3','U7,R6,D4,L4',6",
+    "'R75,D30,R83,U83,L12,D49,R71,U7,L72','U62,R66,U55,R34,D71,R55,D58,R83',159",
+    "'R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51','U98,R91,D20,R16,D67,R40,U7,R15,U6,R7',135"
+  })
+  void examplePart1(final String path1, final String path2, final long result) {
+    final WirePath wp1 = WirePath.of(path1);
+    final WirePath wp2 = WirePath.of(path2);
+    System.out.println(print(wp1, wp2));
+
+    final Point point =
+        Point.shortestManhattanDistanceToCenter(WirePath.intersectionPoints(wp1, wp2));
+    assertEquals(result, point.manhattanDistanceToCenter());
+  }
+}
