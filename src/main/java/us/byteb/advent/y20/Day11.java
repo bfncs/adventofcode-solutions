bb03fb2e65320acad4306c24d5820e14667a0701
Marc Loehe <loehe@sipgate.de>
2020-12-15 11:12:32 +0100

    feat: add y20 d11 p1

---
+++ src/main/java/us/byteb/advent/y20/Day11.java
@@ -0,0 +1,114 @@
+package us.byteb.advent.y20;
+
+import static us.byteb.advent.Utils.readFileFromResources;
+import static us.byteb.advent.y20.Day11.PositionState.*;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Day11 {
+
+  public static void main(String[] args) {
+    final List<List<PositionState>> initialState =
+        parseInput(readFileFromResources("y20/day11.txt"));
+
+    System.out.println("Part 1: " + countOccupiedSeats(findStableState(initialState)));
+  }
+
+  static List<List<PositionState>> parseInput(final String input) {
+    return input
+        .lines()
+        .map(
+            line ->
+                line.chars().mapToObj(c -> PositionState.of((char) c)).collect(Collectors.toList()))
+        .collect(Collectors.toList());
+  }
+
+  static List<List<PositionState>> nextState(final List<List<PositionState>> curState) {
+    final List<List<PositionState>> nextState = new ArrayList<>();
+
+    for (int row = 0; row < curState.size(); row++) {
+      final List<PositionState> curRow = curState.get(row);
+      final List<PositionState> nextRow = new ArrayList<>();
+      for (int col = 0; col < curRow.size(); col++) {
+        final PositionState nextPosition =
+            switch (curRow.get(col)) {
+              case FLOOR -> FLOOR;
+              case EMPTY_SEAT -> countOccupiedSeatsInRow(adjacentSeats(curState, row, col)) == 0
+                  ? OCCUPIED_SEAT
+                  : EMPTY_SEAT;
+              case OCCUPIED_SEAT -> countOccupiedSeatsInRow(adjacentSeats(curState, row, col)) >= 4
+                  ? EMPTY_SEAT
+                  : OCCUPIED_SEAT;
+            };
+
+        nextRow.add(nextPosition);
+      }
+      nextState.add(nextRow);
+    }
+
+    return nextState;
+  }
+
+  static long countOccupiedSeats(final List<List<PositionState>> seats) {
+    return countOccupiedSeatsInRow(
+        seats.stream().flatMap(Collection::stream).collect(Collectors.toList()));
+  }
+
+  static long countOccupiedSeatsInRow(final List<PositionState> seats) {
+    return seats.stream().filter(p -> p == OCCUPIED_SEAT).count();
+  }
+
+  private static List<PositionState> adjacentSeats(
+      final List<List<PositionState>> curState, final int curRow, final int curCol) {
+    final List<PositionState> adjacentSeats = new ArrayList<>();
+
+    for (int row = Math.max(0, curRow - 1);
+        row <= Math.min(curRow + 1, curState.size() - 1);
+        row++) {
+      for (int col = Math.max(0, curCol - 1);
+          col <= Math.min(curCol + 1, curState.get(row).size() - 1);
+          col++) {
+        final PositionState positionState = curState.get(row).get(col);
+        if (!((row == curRow) && (col == curCol))) {
+          adjacentSeats.add(positionState);
+        }
+      }
+    }
+    return adjacentSeats;
+  }
+
+  public static List<List<PositionState>> findStableState(
+      final List<List<PositionState>> initialState) {
+    List<List<PositionState>> state = initialState;
+    while (true) {
+      final List<List<PositionState>> nextState = nextState(state);
+      if (state.equals(nextState)) {
+        return state;
+      }
+      state = nextState;
+    }
+  }
+
+  enum PositionState {
+    FLOOR('.'),
+    EMPTY_SEAT('L'),
+    OCCUPIED_SEAT('#');
+
+    private final char character;
+
+    PositionState(final char character) {
+      this.character = character;
+    }
+
+    static PositionState of(final char character) {
+      return Arrays.stream(values())
+          .filter(p -> p.character == character)
+          .findFirst()
+          .orElseThrow();
+    }
+  }
+}
---
+++ src/test/java/us/byteb/advent/y20/Day11Test.java
@@ -0,0 +1,106 @@
+package us.byteb.advent.y20;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static us.byteb.advent.y20.Day11.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import org.junit.jupiter.api.Test;
+
+class Day11Test {
+
+  List<List<List<PositionState>>> part1ExampleSteps =
+      List.of(
+              """
+      L.LL.LL.LL
+      LLLLLLL.LL
+      L.L.L..L..
+      LLLL.LL.LL
+      L.LL.LL.LL
+      L.LLLLL.LL
+      ..L.L.....
+      LLLLLLLLLL
+      L.LLLLLL.L
+      L.LLLLL.LL
+      """,
+              """
+      #.##.##.##
+      #######.##
+      #.#.#..#..
+      ####.##.##
+      #.##.##.##
+      #.#####.##
+      ..#.#.....
+      ##########
+      #.######.#
+      #.#####.##
+      """,
+              """
+      #.LL.L#.##
+      #LLLLLL.L#
+      L.L.L..L..
+      #LLL.LL.L#
+      #.LL.LL.LL
+      #.LLLL#.##
+      ..L.L.....
+      #LLLLLLLL#
+      #.LLLLLL.L
+      #.#LLLL.##
+      """,
+              """
+      #.##.L#.##
+      #L###LL.L#
+      L.#.#..#..
+      #L##.##.L#
+      #.##.LL.LL
+      #.###L#.##
+      ..#.#.....
+      #L######L#
+      #.LL###L.L
+      #.#L###.##
+      """,
+              """
+      #.#L.L#.##
+      #LLL#LL.L#
+      L.L.L..#..
+      #LLL.##.L#
+      #.LL.LL.LL
+      #.LL#L#.##
+      ..L.L.....
+      #L#LLLL#L#
+      #.LLLLLL.L
+      #.#L#L#.##
+      """,
+              """
+      #.#L.L#.##
+      #LLL#LL.L#
+      L.#.L..#..
+      #L##.##.L#
+      #.#L.LL.LL
+      #.#L#L#.##
+      ..L.L.....
+      #L#L##L#L#
+      #.LLLLLL.L
+      #.#L#L#.##
+      """)
+          .stream()
+          .map(Day11::parseInput)
+          .collect(Collectors.toList());
+
+  @Test
+  void part1ExampleSteps() {
+    assertEquals(part1ExampleSteps.get(1), nextState(part1ExampleSteps.get(0)));
+    assertEquals(part1ExampleSteps.get(2), nextState(part1ExampleSteps.get(1)));
+    assertEquals(part1ExampleSteps.get(3), nextState(part1ExampleSteps.get(2)));
+    assertEquals(part1ExampleSteps.get(4), nextState(part1ExampleSteps.get(3)));
+    assertEquals(part1ExampleSteps.get(5), nextState(part1ExampleSteps.get(4)));
+    assertEquals(part1ExampleSteps.get(5), nextState(part1ExampleSteps.get(5)));
+  }
+
+  @Test
+  void part1ExampleStableState() {
+    final List<List<PositionState>> result = findStableState(part1ExampleSteps.get(0));
+    assertEquals(part1ExampleSteps.get(5), result);
+    assertEquals(37, countOccupiedSeats(result));
+  }
+}
