803b95a312c1cc5cb8154b22ae7cc872a2f7e025
Marc Löhe <loehe@sipgate.de>
2021-12-07 19:40:24 +0100

    feat: add y2021 d3 p2

--- src/main/java/us/byteb/advent/year2021/Day03.java
+++ src/main/java/us/byteb/advent/year2021/Day03.java
@@ -4,44 +4,106 @@ import static us.byteb.advent.Utils.readFileFromResources;
 
 import java.io.IOException;
 import java.util.List;
-import java.util.stream.Collectors;
+import java.util.function.BiFunction;
 
 public class Day03 {
 
   public static void main(String[] args) throws IOException {
-    final List<Integer> input = parseInput(readFileFromResources("year2021/day03.txt"));
+    final PuzzleInput input = parseInput(readFileFromResources("year2021/day03.txt"));
 
-    System.out.println("Part 1: " + (gammaRate(input, 12) * epsilonRate(input, 12)));
+    System.out.println("Part 1: " + (gammaRate(input) * epsilonRate(input)));
+    System.out.println("Part 2: " + (oxygenGeneratorRating(input) * co2ScrubberRating(input)));
   }
 
-  static List<Integer> parseInput(final String input) {
-    return input.lines().map(s -> Integer.parseInt(s, 2)).collect(Collectors.toList());
+  public static PuzzleInput parseInput(final String input) {
+    final List<Integer> numbers = input.lines().map(s -> Integer.parseInt(s, 2)).toList();
+    final int maxBits = input.lines().findFirst().orElseThrow().length();
+    return new PuzzleInput(numbers, maxBits);
   }
 
-  static int gammaRate(final List<Integer> input, int maxBits) {
+  record PuzzleInput(List<Integer> numbers, int maxBits) {}
+
+  public static int gammaRate(final PuzzleInput puzzleInput) {
     int result = 0;
 
-    for (int bit = 0; bit < maxBits; bit++) {
-      long setBitCount = 0;
-      final int mask = 1 << bit;
-      for (final Integer i : input) {
-        if ((i & mask) > 0) {
-          setBitCount++;
-        }
-      }
-
-      if (setBitCount > (input.size() / 2)) {
-        result = result | mask;
+    for (int bit = 0; bit < puzzleInput.maxBits(); bit++) {
+      if (mostlySetBitValue(puzzleInput.numbers(), bit)) {
+        result = result | 1 << bit;
       }
     }
 
     return result;
   }
 
-  static int epsilonRate(final List<Integer> input, final int maxBits) {
-    final int gammaRate = gammaRate(input, maxBits);
-    final int mask = createLeastSignificantBitsMask(maxBits);
+  public static int epsilonRate(final PuzzleInput puzzleInput) {
+    final int gammaRate = gammaRate(puzzleInput);
+    final int mask = createLeastSignificantBitsMask(puzzleInput.maxBits());
     return ~gammaRate & mask;
+  }
+
+  public static int oxygenGeneratorRating(final PuzzleInput puzzleInput) {
+    return filterRating(
+        puzzleInput,
+        (currentNumbers, bitPosition) -> {
+          final boolean mostlySetBitValue = mostlySetBitValue(currentNumbers, bitPosition, true);
+          return onlyNumbersWithBitValueAtPosition(currentNumbers, mostlySetBitValue, bitPosition);
+        });
+  }
+
+  public static int co2ScrubberRating(final PuzzleInput puzzleInput) {
+    return filterRating(
+        puzzleInput,
+        (currentNumbers, bitPosition) -> {
+          final boolean mostlySetBitValue = !mostlySetBitValue(currentNumbers, bitPosition, true);
+          return onlyNumbersWithBitValueAtPosition(currentNumbers, mostlySetBitValue, bitPosition);
+        });
+  }
+
+  private static Integer filterRating(
+      final PuzzleInput puzzleInput,
+      BiFunction<List<Integer>, Integer, List<Integer>> filterStrategy) {
+    List<Integer> currentNumbers = puzzleInput.numbers();
+
+    for (int bitPosition = puzzleInput.maxBits() - 1; bitPosition >= 0; bitPosition--) {
+      currentNumbers = filterStrategy.apply(currentNumbers, bitPosition);
+
+      if (currentNumbers.size() == 1) {
+        return currentNumbers.get(0);
+      }
+    }
+
+    throw new IllegalStateException("Unable to find oxygen generator rating");
+  }
+
+  private static boolean mostlySetBitValue(
+      final List<Integer> input, final int bit, final boolean positiveBias) {
+    long setBitCount = 0;
+    for (final Integer i : input) {
+      if ((i & 1 << bit) > 0) {
+        setBitCount++;
+      }
+    }
+
+    final long unsetBitCount = input.size() - setBitCount;
+    if (setBitCount == unsetBitCount) {
+      return positiveBias;
+    }
+    return setBitCount > unsetBitCount;
+  }
+
+  private static List<Integer> onlyNumbersWithBitValueAtPosition(
+      List<Integer> input, final boolean wantedBitValue, final int bitPosition) {
+    return input.stream()
+        .filter(n -> bitValueAtPosition(bitPosition, n) == wantedBitValue)
+        .toList();
+  }
+
+  private static boolean bitValueAtPosition(final int bitPosition, final Integer n) {
+    return (n & (1 << bitPosition)) > 0;
+  }
+
+  private static boolean mostlySetBitValue(final List<Integer> input, final int bit) {
+    return mostlySetBitValue(input, bit, false);
   }
 
   private static int createLeastSignificantBitsMask(final int numBits) {
--- src/test/java/us/byteb/advent/year2021/Day03Test.java
+++ src/test/java/us/byteb/advent/year2021/Day03Test.java
@@ -3,12 +3,11 @@ package us.byteb.advent.year2021;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static us.byteb.advent.year2021.Day03.*;
 
-import java.util.List;
 import org.junit.jupiter.api.Test;
 
 class Day03Test {
 
-  private static final List<Integer> example1Input =
+  private static final PuzzleInput example1Input =
       parseInput(
           """
            00100
@@ -27,10 +26,19 @@ class Day03Test {
 
   @Test
   void part1Example() {
-    final long gammaRate = gammaRate(example1Input, 5);
-    final long epsilonRate = epsilonRate(example1Input, 5);
+    final long gammaRate = gammaRate(example1Input);
+    final long epsilonRate = epsilonRate(example1Input);
     assertEquals(22, gammaRate);
     assertEquals(9, epsilonRate);
     assertEquals(198, gammaRate * epsilonRate);
+  }
+
+  @Test
+  void part3Example() {
+    final long oxygenGeneratorRating = oxygenGeneratorRating(example1Input);
+    final long co2ScrubberRating = co2ScrubberRating(example1Input);
+    assertEquals(23, oxygenGeneratorRating);
+    assertEquals(10, co2ScrubberRating);
+    assertEquals(230, oxygenGeneratorRating * co2ScrubberRating);
   }
 }
