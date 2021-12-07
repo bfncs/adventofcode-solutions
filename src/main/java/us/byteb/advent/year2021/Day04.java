cd737503806963374d29e83c53a168e94b57807c
Marc Löhe <loehe@sipgate.de>
2021-12-07 22:41:13 +0100

    feat: add y2021 d4 p1

---
+++ src/main/java/us/byteb/advent/year2021/Day04.java
@@ -0,0 +1,117 @@
+package us.byteb.advent.year2021;
+
+import static us.byteb.advent.Utils.readFileFromResources;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+
+public class Day04 {
+
+  public static void main(String[] args) throws IOException {
+    final BingoInput input = parseInput(readFileFromResources("year2021/day04.txt"));
+
+    System.out.println("Part 1: " + scoreOfFirstWinningBoard(input));
+  }
+
+  public static BingoInput parseInput(final String input) {
+    final List<String> lines = input.lines().toList();
+    final List<Integer> drawnNumbers =
+        Arrays.stream(lines.get(0).split(",")).map(Integer::parseInt).toList();
+
+    final List<BingoBoard> boards = new ArrayList<>();
+    List<String> currentBoard = new ArrayList<>();
+    for (int i = 2; i <= lines.size(); i++) {
+      if (i == lines.size() || lines.get(i).isEmpty()) {
+        boards.add(BingoBoard.parse(currentBoard));
+        currentBoard = new ArrayList<>();
+        continue;
+      }
+
+      currentBoard.add(lines.get(i));
+    }
+
+    return new BingoInput(drawnNumbers, boards);
+  }
+
+  public static long scoreOfFirstWinningBoard(final BingoInput input) {
+    final BingoResult result = findFirstWinningBoard(input);
+    return result.firstWinningBoard().score(result.drawnNumbers());
+  }
+
+  public static BingoResult findFirstWinningBoard(final BingoInput input) {
+    for (int i = 0; i < input.drawnNumbers().size(); i++) {
+      final List<Integer> drawnNumbers = input.drawnNumbers().subList(0, i);
+      for (final BingoBoard board : input.boards()) {
+        if (board.isWinning(drawnNumbers)) {
+          return new BingoResult(board, drawnNumbers);
+        }
+      }
+    }
+
+    throw new IllegalStateException("No winning board found!");
+  }
+
+  record BingoResult(BingoBoard firstWinningBoard, List<Integer> drawnNumbers) {}
+
+  record BingoInput(List<Integer> drawnNumbers, List<BingoBoard> boards) {}
+
+  static final class BingoBoard {
+    private final List<List<Integer>> numbers;
+
+    private BingoBoard(final List<List<Integer>> numbers) {
+      this.numbers = numbers;
+    }
+
+    public static BingoBoard parse(final List<String> lines) {
+      final List<List<Integer>> data =
+          lines.stream()
+              .map(str -> Arrays.stream(str.trim().split("\s+")).map(Integer::parseInt).toList())
+              .toList();
+
+      return new BingoBoard(data);
+    }
+
+    public long score(final List<Integer> drawnNumbers) {
+      final long sumOfAllUnmarkedNumbers =
+          numbers.stream()
+              .flatMap(Collection::stream)
+              .filter(n -> !drawnNumbers.contains(n))
+              .mapToLong(n -> n)
+              .sum();
+      final long lastDrawnNumber = drawnNumbers.get(drawnNumbers.size() - 1);
+
+      return sumOfAllUnmarkedNumbers * lastDrawnNumber;
+    }
+
+    public boolean isWinning(final List<Integer> drawnNumbers) {
+      return containsWinningRow(drawnNumbers) || containsWinningColumn(drawnNumbers);
+    }
+
+    private boolean containsWinningRow(final List<Integer> drawnNumbers) {
+      for (final List<Integer> row : numbers) {
+        if (drawnNumbers.containsAll(row)) {
+          return true;
+        }
+      }
+
+      return false;
+    }
+
+    private boolean containsWinningColumn(final List<Integer> drawnNumbers) {
+      for (int numColumn = 0; numColumn < numbers.get(0).size(); numColumn++) {
+        final List<Integer> column = new ArrayList<>();
+        for (final List<Integer> row : numbers) {
+          column.add(row.get(numColumn));
+        }
+
+        if (drawnNumbers.containsAll(column)) {
+          return true;
+        }
+      }
+      return false;
+    }
+  }
+}
