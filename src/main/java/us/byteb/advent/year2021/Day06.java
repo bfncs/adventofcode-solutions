837d46acfc8926c75819616eef766923ea77288b
Marc Löhe <loehe@sipgate.de>
2021-12-08 00:32:51 +0100

    feat: add y2021 d6 p2

--- src/main/java/us/byteb/advent/year2021/Day06.java
+++ src/main/java/us/byteb/advent/year2021/Day06.java
@@ -4,43 +4,55 @@ import static us.byteb.advent.Utils.readFileFromResources;
 
 import java.io.IOException;
 import java.util.*;
-import java.util.stream.Stream;
 
 public class Day06 {
 
   public static void main(String[] args) throws IOException {
     final List<Integer> input = parseInput(readFileFromResources("year2021/day06.txt"));
 
-    System.out.println("Part 1: " + evolveDays(input, 80).size());
+    System.out.println("Part 1: " + totalFishAfterDays(input, 80));
+    System.out.println("Part 2: " + totalFishAfterDays(input, 256));
   }
 
   static List<Integer> parseInput(final String input) {
     return Arrays.stream(input.split(",")).map(Integer::parseInt).toList();
   }
 
-  static List<Integer> evolveDays(final List<Integer> startState, final int numDays) {
-    List<Integer> state = startState;
+  static long totalFishAfterDays(final List<Integer> fishes, final int numDays) {
+    Map<Integer, Long> currentHistogram = createHistogram(fishes);
 
     for (int i = 0; i < numDays; i++) {
-      state = evolveNextDay(state);
+      currentHistogram = evolveNextDay(currentHistogram);
     }
 
-    return state;
+    return currentHistogram.values().stream().mapToLong(l -> l).sum();
   }
 
-  private static List<Integer> evolveNextDay(final List<Integer> state) {
-    final List<Integer> existing = new ArrayList<>();
-    final List<Integer> fresh = new ArrayList<>();
+  private static Map<Integer, Long> createHistogram(final List<Integer> fishes) {
+    final Map<Integer, Long> histogram = new HashMap<>();
 
-    for (final int fish : state) {
-      if (fish == 0) {
-        existing.add(6);
-        fresh.add(8);
+    for (final Integer fish : fishes) {
+      histogram.put(fish, histogram.getOrDefault(fish, 0L) + 1);
+    }
+
+    return histogram;
+  }
+
+  private static Map<Integer, Long> evolveNextDay(final Map<Integer, Long> histogram) {
+    final Map<Integer, Long> result = new HashMap<>();
+
+    for (final Map.Entry<Integer, Long> entry : histogram.entrySet()) {
+      final int age = entry.getKey();
+      final long count = entry.getValue();
+
+      if (age == 0) {
+        result.put(6, result.getOrDefault(6, 0L) + count);
+        result.put(8, count);
       } else {
-        existing.add(fish - 1);
+        result.put(age - 1, result.getOrDefault(age - 1, 0L) + count);
       }
     }
 
-    return Stream.concat(existing.stream(), fresh.stream()).toList();
+    return result;
   }
 }
--- src/test/java/us/byteb/advent/year2021/Day06Test.java
+++ src/test/java/us/byteb/advent/year2021/Day06Test.java
@@ -1,8 +1,8 @@
 package us.byteb.advent.year2021;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static us.byteb.advent.year2021.Day06.evolveDays;
 import static us.byteb.advent.year2021.Day06.parseInput;
+import static us.byteb.advent.year2021.Day06.totalFishAfterDays;
 
 import java.util.List;
 import org.junit.jupiter.api.Test;
@@ -14,7 +14,13 @@ class Day06Test {
   @Test
   void part1Example() {
     final List<Integer> startState = parseInput(exampleData);
-    assertEquals(26L, evolveDays(startState, 18).size());
-    assertEquals(5934L, evolveDays(startState, 80).size());
+    assertEquals(26L, totalFishAfterDays(startState, 18));
+    assertEquals(5934L, totalFishAfterDays(startState, 80));
+  }
+
+  @Test
+  void part2Example() {
+    final List<Integer> startState = parseInput(exampleData);
+    assertEquals(26984457539L, totalFishAfterDays(startState, 256));
   }
 }
