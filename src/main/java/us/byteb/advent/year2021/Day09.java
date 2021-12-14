5d1782e1ac2e0d01d7e05154274615eb6c4e58ca
Marc Löhe <marc@marcloehe.de>
2021-12-14 22:16:28 +0100

    feat: add y2021 d9 p2

--- src/main/java/us/byteb/advent/year2021/Day09.java
+++ src/main/java/us/byteb/advent/year2021/Day09.java
@@ -3,15 +3,18 @@ package us.byteb.advent.year2021;
 import static us.byteb.advent.Utils.readFileFromResources;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Day09 {
+
+  public static final int MAX_BASIN_HEIGHT = 9;
 
   public static void main(String[] args) throws IOException {
     final List<List<Integer>> input = parseInput(readFileFromResources("year2021/day09.txt"));
 
     System.out.println("Part 1: " + riskLevel(findLowPoints(input)));
+    System.out.println("Part 2: " + productOfThreeLargestBasinSizes(input));
   }
 
   static List<Point> findLowPoints(final List<List<Integer>> input) {
@@ -26,11 +29,38 @@ public class Day09 {
         if (col - 1 >= 0 && input.get(row).get(col - 1) <= height) continue; // left
         if (col + 1 < currentRow.size() && input.get(row).get(col + 1) <= height) continue; // right
 
-        lowPoints.add(new Point(row + 1, height));
+        lowPoints.add(new Point(row, col, height));
       }
     }
 
     return lowPoints;
+  }
+
+  static Set<Set<Point>> findBasins(final List<List<Integer>> input) {
+    final List<Point> lowPoints = findLowPoints(input);
+    return lowPoints.stream()
+        .map(lowPoint -> findBasinAround(input, Set.of(lowPoint)))
+        .collect(Collectors.toSet());
+  }
+
+  private static Set<Point> findBasinAround(
+      final List<List<Integer>> input, final Set<Point> knownPoints) {
+    System.out.println("knownPoints = " + knownPoints);
+    final Set<Point> nextPoints = new HashSet<>(knownPoints);
+    for (final Point point : knownPoints) {
+      final int row = point.row();
+      final int col = point.col();
+      if (row - 1 >= 0 && input.get(row - 1).get(col) < MAX_BASIN_HEIGHT) // up
+      nextPoints.add(new Point(row - 1, col, input.get(row - 1).get(col)));
+      if (row + 1 < input.size() && input.get(row + 1).get(col) < MAX_BASIN_HEIGHT) // down
+      nextPoints.add(new Point(row + 1, col, input.get(row + 1).get(col)));
+      if (col - 1 >= 0 && input.get(row).get(col - 1) < MAX_BASIN_HEIGHT) // left
+      nextPoints.add(new Point(row, col - 1, input.get(row).get(col - 1)));
+      if (col + 1 < input.get(0).size() && input.get(row).get(col + 1) < MAX_BASIN_HEIGHT) // right
+      nextPoints.add(new Point(row, col + 1, input.get(row).get(col + 1)));
+    }
+
+    return nextPoints.equals(knownPoints) ? nextPoints : findBasinAround(input, nextPoints);
   }
 
   static long riskLevel(final List<Point> points) {
@@ -44,5 +74,13 @@ public class Day09 {
         .toList();
   }
 
-  record Point(int row, int height) {}
+  static long productOfThreeLargestBasinSizes(final List<List<Integer>> input) {
+    return findBasins(input).stream()
+        .map(ps -> (long) ps.size())
+        .sorted(Comparator.reverseOrder())
+        .limit(3)
+        .reduce(1L, (a, b) -> a * b);
+  }
+
+  record Point(int row, int col, int height) {}
 }
--- src/test/java/us/byteb/advent/year2021/Day09Test.java
+++ src/test/java/us/byteb/advent/year2021/Day09Test.java
@@ -4,8 +4,8 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static us.byteb.advent.year2021.Day09.*;
 
 import java.util.List;
+import java.util.Set;
 import org.junit.jupiter.api.Test;
-import us.byteb.advent.year2021.Day09.Point;
 
 class Day09Test {
 
@@ -21,8 +21,58 @@ class Day09Test {
   @Test
   void part1Example() {
     final List<Point> lowPoints = findLowPoints(parseInput(exampleInput));
-    assertEquals(
-        List.of(new Point(1, 1), new Point(1, 0), new Point(3, 5), new Point(5, 5)), lowPoints);
+    assertEquals(List.of(p(0, 1, 1), p(0, 9, 0), p(2, 2, 5), p(4, 6, 5)), lowPoints);
     assertEquals(15L, riskLevel(lowPoints));
+  }
+
+  @Test
+  void part2Example() {
+    final Set<Set<Point>> basins = findBasins(parseInput(exampleInput));
+    assertEquals(
+        Set.of(
+            Set.of(p(0, 0, 2), p(0, 1, 1), p(1, 0, 3)),
+            Set.of(
+                p(0, 5, 4),
+                p(0, 6, 3),
+                p(0, 7, 2),
+                p(0, 8, 1),
+                p(0, 9, 0),
+                p(1, 6, 4),
+                p(1, 8, 2),
+                p(1, 9, 1),
+                p(2, 9, 2)),
+            Set.of(
+                p(1, 2, 8),
+                p(1, 3, 7),
+                p(1, 4, 8),
+                p(2, 1, 8),
+                p(2, 2, 5),
+                p(2, 3, 6),
+                p(2, 4, 7),
+                p(2, 5, 8),
+                p(3, 0, 8),
+                p(3, 1, 7),
+                p(3, 2, 6),
+                p(3, 3, 7),
+                p(3, 4, 8),
+                p(4, 1, 8)),
+            Set.of(
+                p(2, 7, 8),
+                p(3, 6, 6),
+                p(3, 7, 7),
+                p(3, 8, 8),
+                p(4, 5, 6),
+                p(4, 6, 5),
+                p(4, 7, 6),
+                p(4, 8, 7),
+                p(4, 9, 8))),
+        basins);
+
+    assertEquals(1134L, productOfThreeLargestBasinSizes(parseInput(exampleInput)));
+    ;
+  }
+
+  private static Point p(final int i, final int i2, final int i3) {
+    return new Point(i, i2, i3);
   }
 }
