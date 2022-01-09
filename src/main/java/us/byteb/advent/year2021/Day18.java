d432cfc6ad85d7ca3141fe5dfd124a5f554fc74a
Marc Löhe <loehe@sipgate.de>
2022-01-09 06:19:04 +0100

    feat: add y2021 d18 p1

---
+++ src/main/java/us/byteb/advent/year2021/Day18.java
@@ -0,0 +1,347 @@
+package us.byteb.advent.year2021;
+
+import static us.byteb.advent.Utils.readFileFromResources;
+
+import java.io.IOException;
+import java.util.*;
+import java.util.function.BiPredicate;
+import java.util.stream.Stream;
+
+public class Day18 {
+
+  public static void main(String[] args) throws IOException {
+    final String input = readFileFromResources("year2021/day18.txt");
+    System.out.println("Part 1: " + SnailfishNumber.sum(parseInput(input)).magnitude());
+  }
+
+  static List<SnailfishNumber> parseInput(final String input) {
+    return input.lines().map(SnailfishNumber::parse).toList();
+  }
+
+  interface SnailfishNumber {
+
+    static Node node(final SnailfishNumber left, final SnailfishNumber right) {
+      return new Node(left, right);
+    }
+
+    static Leaf leaf(final int value) {
+      return new Leaf(value);
+    }
+
+    static SnailfishNumber parse(final String input) {
+      return parseImpl(input).number();
+    }
+
+    private static ParseResult parseImpl(final String input) {
+      int pos = 0;
+      int expressionStart = 0;
+      int stack = 0;
+      SnailfishNumber left = null;
+
+      while (pos < input.length()) {
+        switch (input.charAt(pos)) {
+          case '[' -> {
+            if (stack == 0) {
+              expressionStart = pos + 1;
+            }
+            stack++;
+            pos++;
+          }
+          case ',' -> {
+            if (stack <= 1) {
+              final ParseResult result = parseImpl(input.substring(expressionStart, pos));
+              left = result.number();
+              pos++;
+              expressionStart = pos;
+            } else {
+              pos++;
+            }
+          }
+          case ']' -> {
+            if (stack == 1) {
+              if (left == null) throw new IllegalStateException();
+              final ParseResult right = parseImpl(input.substring(expressionStart, pos));
+              return new ParseResult(
+                  new Node(left, right.number()), expressionStart + right.lastPos());
+            } else {
+              stack--;
+              pos++;
+            }
+          }
+          default -> {
+            pos++;
+          }
+        }
+      }
+
+      return new ParseResult(new Leaf(Integer.parseInt(input)), input.length() - 1);
+    }
+
+    SnailfishNumber reduce();
+
+    static SnailfishNumber sum(final SnailfishNumber a, final SnailfishNumber b) {
+      return new Node(a, b).reduce();
+    }
+
+    static SnailfishNumber sum(List<SnailfishNumber> snailfishNumbers) {
+      SnailfishNumber result = snailfishNumbers.get(0);
+
+      for (int i = 1; i < snailfishNumbers.size(); i++) {
+        result = sum(result, snailfishNumbers.get(i));
+      }
+
+      return result;
+    }
+
+    long magnitude();
+
+    record ParseResult(SnailfishNumber number, int lastPos) {}
+
+    final class Node implements SnailfishNumber {
+      private SnailfishNumber left;
+      private SnailfishNumber right;
+
+      public Node(SnailfishNumber left, SnailfishNumber right) {
+        this.left = left;
+        this.right = right;
+      }
+
+      @Override
+      public SnailfishNumber reduce() {
+        boolean actionApplied;
+        do {
+          actionApplied = false;
+
+          final Optional<List<SnailfishNumber>> explodeResult = findLeftmostNumberNestedInPairs(4);
+          if (explodeResult.isPresent()) {
+            actionApplied = true;
+            explode(explodeResult.get());
+            continue;
+          }
+
+          final Optional<List<SnailfishNumber>> splitResult =
+              findLeftmostLeafGreaterThanOrEqual(10);
+          if (splitResult.isPresent()) {
+            actionApplied = true;
+
+            split(splitResult.get());
+          }
+
+        } while (actionApplied);
+
+        return this;
+      }
+
+      @Override
+      public long magnitude() {
+        return (3 * left.magnitude()) + (2 * right.magnitude());
+      }
+
+      private void split(final List<SnailfishNumber> pathToTarget) {
+        final Leaf target = (Leaf) pathToTarget.get(pathToTarget.size() - 1);
+        final Node parent = (Node) pathToTarget.get(pathToTarget.size() - 2);
+
+        final Leaf left = new Leaf(target.value() / 2);
+        final Leaf right = new Leaf((int) Math.ceil((double) target.value() / 2));
+        final Node replacement = new Node(left, right);
+
+        if (parent.left().equals(target)) {
+          parent.setLeft(replacement);
+        } else {
+          parent.setRight(replacement);
+        }
+      }
+
+      private void explode(final List<SnailfishNumber> pathToTarget) {
+        final Node target = (Node) pathToTarget.get(pathToTarget.size() - 1);
+        final Node parent = (Node) pathToTarget.get(pathToTarget.size() - 2);
+
+        final Optional<Leaf> leftNeighbor = findLeftNeighbor(pathToTarget);
+        leftNeighbor.ifPresent(l -> l.setValue(l.value() + ((Leaf) target.left()).value()));
+
+        final Optional<Leaf> rightNeighbor = findRightNeighbor(pathToTarget);
+        rightNeighbor.ifPresent(l -> l.setValue(l.value() + ((Leaf) target.right()).value()));
+
+        if (parent.left().equals(target)) {
+          parent.setLeft(new Leaf(0));
+        } else {
+          parent.setRight(new Leaf(0));
+        }
+      }
+
+      private Optional<Leaf> findLeftNeighbor(final List<SnailfishNumber> pathToTarget) {
+        SnailfishNumber current = pathToTarget.get(pathToTarget.size() - 1);
+        boolean backtracking = true;
+
+        while (true) {
+          if (backtracking) {
+            final int index = pathToTarget.indexOf(current);
+            if (index == 0) return Optional.empty();
+            final Node parent = (Node) pathToTarget.get(index - 1);
+            if (parent.right() == current) {
+              current = parent.left();
+              backtracking = false;
+            } else {
+              current = parent;
+            }
+          } else {
+            if (current instanceof Node node) {
+              current = node.right();
+            } else if (current instanceof Leaf leaf) {
+              return Optional.of(leaf);
+            }
+          }
+        }
+      }
+
+      private Optional<Leaf> findRightNeighbor(final List<SnailfishNumber> pathToTarget) {
+        SnailfishNumber current = pathToTarget.get(pathToTarget.size() - 1);
+        boolean backtracking = true;
+
+        while (true) {
+          if (backtracking) {
+            final int index = pathToTarget.indexOf(current);
+            if (index == 0) return Optional.empty();
+            final Node parent = (Node) pathToTarget.get(index - 1);
+            if (parent.left() == current) {
+              current = parent.right();
+              backtracking = false;
+            } else {
+              current = parent;
+            }
+          } else {
+            if (current instanceof Node node) {
+              current = node.left();
+            } else if (current instanceof Leaf leaf) {
+              return Optional.of(leaf);
+            }
+          }
+        }
+      }
+
+      Optional<List<SnailfishNumber>> findLeftmostLeafGreaterThanOrEqual(final int limit) {
+        return findLeftmostMatch(
+            (current, stack) -> current instanceof Leaf leaf && leaf.value() >= limit);
+      }
+
+      Optional<List<SnailfishNumber>> findLeftmostNumberNestedInPairs(final int numNestedPairs) {
+        return findLeftmostMatch((current, stack) -> stack.size() >= numNestedPairs + 1)
+            .map(path -> path.subList(0, path.size() - 1));
+      }
+
+      private Optional<List<SnailfishNumber>> findLeftmostMatch(
+          final BiPredicate<SnailfishNumber, Stack<SnailfishNumber>> isMatch) {
+        record NumberWithParents(SnailfishNumber number, List<SnailfishNumber> parents) {}
+        final Set<NumberWithParents> visited = new HashSet<>();
+        final Stack<SnailfishNumber> stack = new Stack<>();
+        SnailfishNumber current = this;
+
+        while (true) {
+
+          if (isMatch.test(current, stack)) {
+            return Optional.of(Stream.concat(stack.stream(), Stream.of(current)).toList());
+          }
+
+          if (current instanceof Node node) {
+            if (!visited.contains(
+                new NumberWithParents(
+                    node.left(), Stream.concat(stack.stream(), Stream.of(current)).toList()))) {
+              stack.push(current);
+              current = node.left();
+            } else if (!visited.contains(
+                new NumberWithParents(
+                    node.right(), Stream.concat(stack.stream(), Stream.of(current)).toList()))) {
+              stack.push(current);
+              current = node.right();
+            } else {
+              if (stack.empty()) return Optional.empty();
+              visited.add(new NumberWithParents(current, stack.stream().toList()));
+              current = stack.pop();
+            }
+          } else if (current instanceof Leaf leaf) {
+            visited.add(new NumberWithParents(current, stack.stream().toList()));
+            current = stack.pop();
+          }
+        }
+      }
+
+      public SnailfishNumber left() {
+        return left;
+      }
+
+      public SnailfishNumber right() {
+        return right;
+      }
+
+      public void setLeft(final SnailfishNumber left) {
+        this.left = left;
+      }
+
+      public void setRight(final SnailfishNumber right) {
+        this.right = right;
+      }
+
+      @Override
+      public boolean equals(Object obj) {
+        if (obj == this) return true;
+        if (obj == null || obj.getClass() != this.getClass()) return false;
+        var that = (Node) obj;
+        return Objects.equals(this.left, that.left) && Objects.equals(this.right, that.right);
+      }
+
+      @Override
+      public int hashCode() {
+        return Objects.hash(left, right);
+      }
+
+      @Override
+      public String toString() {
+        return "[" + left + "," + right + ']';
+      }
+    }
+
+    final class Leaf implements SnailfishNumber {
+      private int value;
+
+      public Leaf(int value) {
+        this.value = value;
+      }
+
+      @Override
+      public SnailfishNumber reduce() {
+        throw new UnsupportedOperationException();
+      }
+
+      @Override
+      public long magnitude() {
+        return value;
+      }
+
+      public int value() {
+        return value;
+      }
+
+      public void setValue(final int value) {
+        this.value = value;
+      }
+
+      @Override
+      public boolean equals(Object obj) {
+        if (obj == this) return true;
+        if (obj == null || obj.getClass() != this.getClass()) return false;
+        var that = (Leaf) obj;
+        return this.value == that.value;
+      }
+
+      @Override
+      public int hashCode() {
+        return Objects.hash(value);
+      }
+
+      @Override
+      public String toString() {
+        return String.valueOf(value);
+      }
+    }
+  }
+}
---
+++ src/test/java/us/byteb/advent/year2021/Day18Test.java
@@ -0,0 +1,170 @@
+package us.byteb.advent.year2021;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
+import static us.byteb.advent.year2021.Day18.SnailfishNumber.leaf;
+import static us.byteb.advent.year2021.Day18.SnailfishNumber.node;
+import static us.byteb.advent.year2021.Day18.parseInput;
+
+import java.util.stream.Stream;
+import org.junit.jupiter.api.extension.ExtensionContext;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.ArgumentsProvider;
+import org.junit.jupiter.params.provider.ArgumentsSource;
+import us.byteb.advent.year2021.Day18.*;
+
+class Day18Test {
+
+  @ParameterizedTest
+  @ArgumentsSource(ParseExamples.class)
+  void parse(final String input, final SnailfishNumber expectedResult) {
+    assertEquals(expectedResult, SnailfishNumber.parse(input));
+  }
+
+  static class ParseExamples implements ArgumentsProvider {
+
+    @Override
+    public Stream<? extends Arguments> provideArguments(final ExtensionContext context) {
+      return Stream.of(
+          arguments("[1,2]", node(leaf(1), leaf(2))),
+          arguments("[[1,2],3]", node(node(leaf(1), leaf(2)), leaf(3))),
+          arguments("[[1,9],[8,5]]", node(node(leaf(1), leaf(9)), node(leaf(8), leaf(5)))),
+          arguments(
+              "[[[[1,2],[3,4]],[[5,6],[7,8]]],9]",
+              node(
+                  node(
+                      node(node(leaf(1), leaf(2)), node(leaf(3), leaf(4))),
+                      node(node(leaf(5), leaf(6)), node(leaf(7), leaf(8)))),
+                  leaf(9))),
+          arguments(
+              "[[[9,[3,8]],[[0,9],6]],[[[3,7],[4,9]],3]]",
+              node(
+                  node(
+                      node(leaf(9), node(leaf(3), leaf(8))), node(node(leaf(0), leaf(9)), leaf(6))),
+                  node(node(node(leaf(3), leaf(7)), node(leaf(4), leaf(9))), leaf(3)))),
+          arguments(
+              "[[[[1,3],[5,3]],[[1,3],[8,7]]],[[[4,9],[6,9]],[[8,2],[7,3]]]]",
+              node(
+                  node(
+                      node(node(leaf(1), leaf(3)), node(leaf(5), leaf(3))),
+                      node(node(leaf(1), leaf(3)), node(leaf(8), leaf(7)))),
+                  node(
+                      node(node(leaf(4), leaf(9)), node(leaf(6), leaf(9))),
+                      node(node(leaf(8), leaf(2)), node(leaf(7), leaf(3)))))));
+    }
+  }
+
+  @ParameterizedTest
+  @ArgumentsSource(ReduceExamples.class)
+  void reduce(final SnailfishNumber input, final SnailfishNumber expectedResult) {
+    assertEquals(expectedResult, input.reduce());
+  }
+
+  static class ReduceExamples implements ArgumentsProvider {
+
+    @Override
+    public Stream<? extends Arguments> provideArguments(final ExtensionContext context) {
+      return Stream.of(
+          arguments(
+              SnailfishNumber.parse("[[[[[9,8],1],2],3],4]"),
+              SnailfishNumber.parse("[[[[0,9],2],3],4]")),
+          arguments(
+              SnailfishNumber.parse("[7,[6,[5,[4,[3,2]]]]]"),
+              SnailfishNumber.parse("[7,[6,[5,[7,0]]]]")),
+          arguments(
+              SnailfishNumber.parse("[[6,[5,[4,[3,2]]]],1]"),
+              SnailfishNumber.parse("[[6,[5,[7,0]]],3]")),
+          arguments(
+              SnailfishNumber.parse("[[3,[2,[1,[7,3]]]],[6,[5,[4,[3,2]]]]]"),
+              SnailfishNumber.parse("[[3,[2,[8,0]]],[9,[5,[7,0]]]]")),
+          arguments(SnailfishNumber.parse("[1,10]"), SnailfishNumber.parse("[1,[5,5]]")),
+          arguments(SnailfishNumber.parse("[10,1]"), SnailfishNumber.parse("[[5,5],1]")),
+          arguments(SnailfishNumber.parse("[1,11]"), SnailfishNumber.parse("[1,[5,6]]")),
+          arguments(SnailfishNumber.parse("[11,1]"), SnailfishNumber.parse("[[5,6],1]")),
+          arguments(
+              SnailfishNumber.parse("[[[[[4,3],4],4],[7,[[8,4],9]]],[1,1]]"),
+              SnailfishNumber.parse("[[[[0,7],4],[[7,8],[6,0]]],[8,1]]")),
+          arguments(
+              SnailfishNumber.sum(
+                  SnailfishNumber.parse("[[[[4,3],4],4],[7,[[8,4],9]]]"),
+                  SnailfishNumber.parse("[1,1]")),
+              SnailfishNumber.parse("[[[[0,7],4],[[7,8],[6,0]]],[8,1]]")));
+    }
+  }
+
+  @ParameterizedTest
+  @ArgumentsSource(SumListExamples.class)
+  void sumList(final String input, final String expectedResult) {
+    assertEquals(SnailfishNumber.parse(expectedResult), SnailfishNumber.sum(parseInput(input)));
+  }
+
+  static class SumListExamples implements ArgumentsProvider {
+
+    @Override
+    public Stream<? extends Arguments> provideArguments(final ExtensionContext context) {
+      return Stream.of(
+          arguments(
+              """
+              [1,1]
+              [2,2]
+              [3,3]
+              [4,4]
+              """,
+              "[[[[1,1],[2,2]],[3,3]],[4,4]]"),
+          arguments(
+              """
+              [1,1]
+              [2,2]
+              [3,3]
+              [4,4]
+              [5,5]
+              """,
+              "[[[[3,0],[5,3]],[4,4]],[5,5]]"),
+          arguments(
+              """
+              [1,1]
+              [2,2]
+              [3,3]
+              [4,4]
+              [5,5]
+              [6,6]
+              """,
+              "[[[[5,0],[7,4]],[5,5]],[6,6]]"),
+          arguments(
+              """
+              [[[0,[4,5]],[0,0]],[[[4,5],[2,6]],[9,5]]]
+              [7,[[[3,7],[4,3]],[[6,3],[8,8]]]]
+              [[2,[[0,8],[3,4]]],[[[6,7],1],[7,[1,6]]]]
+              [[[[2,4],7],[6,[0,5]]],[[[6,8],[2,8]],[[2,1],[4,5]]]]
+              [7,[5,[[3,8],[1,4]]]]
+              [[2,[2,2]],[8,[8,1]]]
+              [2,9]
+              [1,[[[9,3],9],[[9,0],[0,7]]]]
+              [[[5,[7,4]],7],1]
+              [[[[4,2],2],6],[8,7]]
+              """,
+              "[[[[8,7],[7,7]],[[8,6],[7,7]]],[[[0,7],[6,6]],[8,7]]]"));
+    }
+  }
+
+  @ParameterizedTest
+  @ArgumentsSource(MagnitudeExamples.class)
+  void magnitude(final String input, final long expectedResult) {
+    assertEquals(expectedResult, SnailfishNumber.parse(input).magnitude());
+  }
+
+  static class MagnitudeExamples implements ArgumentsProvider {
+
+    @Override
+    public Stream<? extends Arguments> provideArguments(final ExtensionContext context) {
+      return Stream.of(
+          arguments("[[1,2],[[3,4],5]]", 143),
+          arguments("[[[[0,7],4],[[7,8],[6,0]]],[8,1]]", 1384),
+          arguments("[[[[1,1],[2,2]],[3,3]],[4,4]]", 445),
+          arguments("[[[[3,0],[5,3]],[4,4]],[5,5]]", 791),
+          arguments("[[[[5,0],[7,4]],[5,5]],[6,6]]", 1137),
+          arguments("[[[[8,7],[7,7]],[[8,6],[7,7]]],[[[0,7],[6,6]],[8,7]]]", 3488));
+    }
+  }
+}
