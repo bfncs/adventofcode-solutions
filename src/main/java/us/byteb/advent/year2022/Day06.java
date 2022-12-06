780cb95058fb37da7a116d3f641138da2d982cde
Marc Löhe <marc@marcloehe.de>
2022-12-06 07:52:47 +0100

    feat: add y2022 d6 p2

--- src/main/java/us/byteb/advent/year2022/Day06.java
+++ src/main/java/us/byteb/advent/year2022/Day06.java
@@ -7,22 +7,32 @@ import java.util.stream.IntStream;
 
 public class Day06 {
 
-  public static final int START_MARKER_LENGHT = 4;
+  public static final int PACKET_START_MARKER_LENGTH = 4;
+  public static final int MESSAGE_START_MARKER_LENGTH = 14;
 
   public static void main(String[] args) {
     final String input = readFileFromResources("year2022/day06.txt");
 
-    System.out.println("Part 1: " + findPosAfterFirstStartMarker(input));
+    System.out.println("Part 1: " + findPosAfterFirstPacketStartMarker(input));
+    System.out.println("Part 2: " + findPosAfterFirstMessageStartMarker(input));
   }
 
-  static int findPosAfterFirstStartMarker(final String input) {
-    for (int i = START_MARKER_LENGHT; i < input.length(); i++) {
+  static int findPosAfterFirstPacketStartMarker(final String input) {
+    return findPosAfterMarker(input, PACKET_START_MARKER_LENGTH);
+  }
+
+  static int findPosAfterFirstMessageStartMarker(final String input) {
+    return findPosAfterMarker(input, MESSAGE_START_MARKER_LENGTH);
+  }
+
+  private static int findPosAfterMarker(final String input, final int distinctCharactersLength) {
+    for (int i = distinctCharactersLength; i < input.length(); i++) {
       final int uniqChars =
-          IntStream.range(i - START_MARKER_LENGHT, i)
+          IntStream.range(i - distinctCharactersLength, i)
               .mapToObj(input::charAt)
               .collect(Collectors.toSet())
               .size();
-      if (uniqChars == START_MARKER_LENGHT) {
+      if (uniqChars == distinctCharactersLength) {
         return i;
       }
     }
