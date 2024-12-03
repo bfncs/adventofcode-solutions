83e3024e825cd7f163910d33608a61ab2984da75
Marc Löhe <loehe@sipgate.de>
2024-12-03 21:18:18 +0100

    feat: add y2024 d3 p2

--- src/main/java/us/byteb/advent/year2024/Day03.java
+++ src/main/java/us/byteb/advent/year2024/Day03.java
@@ -9,34 +9,62 @@ import java.util.regex.Pattern;
 
 public class Day03 {
 
-  private static final Pattern PATTERN = Pattern.compile("mul\\((\\d{1,3}),(\\d{1,3})\\)");
+  private static final Pattern PATTERN =
+      Pattern.compile("mul\\((\\d{1,3}),(\\d{1,3})\\)|do\\(\\)|don't\\(\\)");
 
   public static void main(String[] args) {
     final String input = readFileFromResources("year2024/day03.txt");
 
-    System.out.println("Part 1: " + sum(parseInput(input)));
+    System.out.println("Part 1: " + evaluate(parseInput(input), true));
+    System.out.println("Part 2: " + evaluate(parseInput(input), false));
   }
 
-  static List<Mul> parseInput(final String input) {
-    final List<Mul> result = new ArrayList<>();
+  static List<Op> parseInput(final String input) {
+    final List<Op> result = new ArrayList<>();
     final Matcher matcher = PATTERN.matcher(input);
     while (matcher.find()) {
-      final long left = Long.parseLong(matcher.group(1));
-      final long right = Long.parseLong(matcher.group(2));
-      result.add(new Mul(left, right));
+      switch (matcher.group(0)) {
+        case "do()" -> result.add(new Op.Do());
+        case "don't()" -> result.add(new Op.Dont());
+        default -> {
+          final long left = Long.parseLong(matcher.group(1));
+          final long right = Long.parseLong(matcher.group(2));
+          result.add(new Op.Mul(left, right));
+        }
+      }
     }
 
     return result;
   }
 
-  record Mul(long left, long right) {
-
-    public long eval() {
-      return left * right;
+  sealed interface Op {
+    record Mul(long left, long right) implements Op {
+      public long eval() {
+        return left * right;
+      }
     }
+
+    record Do() implements Op {}
+
+    record Dont() implements Op {}
   }
 
-  static long sum(final List<Mul> instructions) {
-    return instructions.stream().mapToLong(Mul::eval).sum();
+  static long evaluate(final List<Op> instructions, final boolean ignoreToggle) {
+    long sum = 0;
+    boolean instructionsEnabled = true;
+
+    for (final Op instruction : instructions) {
+      switch (instruction) {
+        case Op.Do ignored -> instructionsEnabled = true;
+        case Op.Dont ignored -> instructionsEnabled = false;
+        case Op.Mul mul -> {
+          if (ignoreToggle || instructionsEnabled) {
+            sum += mul.eval();
+          }
+        }
+      }
+    }
+
+    return sum;
   }
 }
--- src/test/java/us/byteb/advent/year2024/Day03Test.java
+++ src/test/java/us/byteb/advent/year2024/Day03Test.java
@@ -8,16 +8,37 @@ import org.junit.jupiter.api.Test;
 
 class Day03Test {
 
-  private static final String EXAMPLE_DATA =
+  private static final String EXAMPLE_PART1 =
       """
       xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))
       """;
 
+  private static final String EXAMPLE_PART2 =
+      """
+      xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))
+      """;
+
   @Test
   void partOneExample() {
-    final List<Mul> instructions = parseInput(EXAMPLE_DATA);
+    final List<Op> instructions = parseInput(EXAMPLE_PART1);
     assertEquals(
-        List.of(new Mul(2, 4), new Mul(5, 5), new Mul(11, 8), new Mul(8, 5)), instructions);
-    assertEquals(161, sum(instructions));
+        List.of(new Op.Mul(2, 4), new Op.Mul(5, 5), new Op.Mul(11, 8), new Op.Mul(8, 5)),
+        instructions);
+    assertEquals(161, evaluate(instructions, true));
+  }
+
+  @Test
+  void partTwoExample() {
+    final List<Op> instructions = parseInput(EXAMPLE_PART2);
+    assertEquals(
+        List.of(
+            new Op.Mul(2, 4),
+            new Op.Dont(),
+            new Op.Mul(5, 5),
+            new Op.Mul(11, 8),
+            new Op.Do(),
+            new Op.Mul(8, 5)),
+        instructions);
+    assertEquals(48, evaluate(instructions, false));
   }
 }
