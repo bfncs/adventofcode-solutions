da7fe6f00d4019a7361b80ae7c112cf9663a586e
Marc Löhe <loehe@sipgate.de>
2024-12-08 21:43:20 +0100

    feat: add y2024 d7 p1

---
+++ src/main/java/us/byteb/advent/year2024/Day07.java
@@ -0,0 +1,77 @@
+package us.byteb.advent.year2024;
+
+import static us.byteb.advent.Utils.readFileFromResources;
+
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class Day07 {
+
+  public static void main(String[] args) {
+    final List<Equation> input = parseInput(readFileFromResources("year2024/day07.txt"));
+    System.out.println("Part 1: " + totalCalibrationResult(input));
+  }
+
+  public static List<Equation> parseInput(final String input) {
+    return input.lines().map(Equation::parse).toList();
+  }
+
+  public static long totalCalibrationResult(final List<Equation> equations) {
+    return equations.stream()
+        .filter(Equation::canBeSolved)
+        .mapToLong(Equation::expectedResult)
+        .sum();
+  }
+
+  public record Equation(long expectedResult, List<Long> operands) {
+    public static Equation parse(final String input) {
+      final String[] parts = input.split(":\\s*");
+      if (parts.length != 2) throw new IllegalStateException();
+      final List<Long> operands =
+          Arrays.stream(parts[1].split("\\s+")).map(Long::parseLong).toList();
+      return new Equation(Long.parseLong(parts[0]), operands);
+    }
+
+    boolean canBeSolved() {
+      final List<Op> ops = Arrays.stream(Op.values()).toList();
+      final Set<List<Op>> opCombinations = combinations(ops, operands().size() - 1);
+      for (final List<Op> combination : opCombinations) {
+        long result = operands().getFirst();
+        for (int i = 0; i < operands.size() - 1; i++) {
+          result =
+              switch (combination.get(i)) {
+                case ADD -> result + operands.get(i + 1);
+                case MULTIPLY -> result * operands.get(i + 1);
+              };
+        }
+        if (result == expectedResult) {
+          return true;
+        }
+      }
+      return false;
+    }
+
+    private static <T> Set<List<T>> combinations(final Collection<T> items, int length) {
+      if (length == 0) return Set.of(List.of());
+      return items.stream()
+          .flatMap(
+              item -> {
+                final Set<List<T>> combinations = combinations(items, length - 1);
+                return combinations.stream()
+                    .map(
+                        tail -> {
+                          final List<T> list =
+                              Stream.concat(Stream.of(item), tail.stream()).toList();
+                          return list;
+                        });
+              })
+          .collect(Collectors.toSet());
+    }
+  }
+
+  enum Op {
+    ADD,
+    MULTIPLY
+  }
+}
---
+++ src/test/java/us/byteb/advent/year2024/Day07Test.java
@@ -0,0 +1,27 @@
+package us.byteb.advent.year2024;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static us.byteb.advent.year2024.Day07.*;
+
+import org.junit.jupiter.api.Test;
+
+class Day07Test {
+
+  private static final String EXAMPLE_DATA =
+      """
+      190: 10 19
+      3267: 81 40 27
+      83: 17 5
+      156: 15 6
+      7290: 6 8 6 15
+      161011: 16 10 13
+      192: 17 8 14
+      21037: 9 7 18 13
+      292: 11 6 16 20
+      """;
+
+  @Test
+  void partOneExample() {
+    assertEquals(3749L, totalCalibrationResult(parseInput(EXAMPLE_DATA)));
+  }
+}
