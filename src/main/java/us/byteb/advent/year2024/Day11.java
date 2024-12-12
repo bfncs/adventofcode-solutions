69bd74e9c531f6a12127faa1f6c4a397c7a8a199
Marc Löhe <loehe@sipgate.de>
2024-12-12 12:01:35 +0100

    feat: add y2024 d11 p2

--- src/main/java/us/byteb/advent/year2024/Day11.java
+++ src/main/java/us/byteb/advent/year2024/Day11.java
@@ -3,19 +3,54 @@ package us.byteb.advent.year2024;
 import static us.byteb.advent.Utils.readFileFromResources;
 
 import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class Day11 {
+  private static final Map<String, Long> cache = new ConcurrentHashMap<>();
 
   public static void main(String[] args) {
     final String input = readFileFromResources("year2024/day11.txt");
-    System.out.println("Part 1: " + blink(parse(input), 25).size());
+    System.out.println("Part 1: " + depthFirstBlink(parse(input), 25));
+    System.out.println("Part 2: " + depthFirstBlink(parse(input), 75));
   }
 
   public static List<Long> parse(final String input) {
     return Arrays.stream(input.split("\\s+")).map(Long::parseLong).toList();
   }
 
-  public static List<Long> blink(final List<Long> stones, final int times) {
+  public static long depthFirstBlink(final List<Long> stones, final int times) {
+    return stones.stream().mapToLong(stone -> depthFirstBlink(stone, times)).sum();
+  }
+
+  public static long depthFirstBlink(final long stone, final int times) {
+    final String key = stone + "-" + times;
+    if (cache.containsKey(key)) return cache.get(key);
+    final long result = depthFirstBlinkImpl(stone, times);
+    cache.put(key, result);
+    return result;
+  }
+
+  public static long depthFirstBlinkImpl(final long stone, final int times) {
+    if (times == 0) {
+      return 1;
+    }
+
+    if (stone == 0) {
+      return depthFirstBlink(1, times - 1);
+    }
+
+    final String str = String.valueOf(stone);
+    if (str.length() % 2 == 0) {
+      final long firstHalf = Long.parseLong(str.substring(0, str.length() / 2), 10);
+      final long secondHalf = Long.parseLong(str.substring(str.length() / 2), 10);
+
+      return depthFirstBlink(firstHalf, times - 1) + depthFirstBlink(secondHalf, times - 1);
+    } else {
+      return depthFirstBlink(stone * 2024, times - 1);
+    }
+  }
+
+  public static List<Long> breadthFirstBlink(final List<Long> stones, final int times) {
     List<Long> result = stones;
     for (int i = 0; i < times; i++) {
       result = blink(result);
--- src/test/java/us/byteb/advent/year2024/Day11Test.java
+++ src/test/java/us/byteb/advent/year2024/Day11Test.java
@@ -9,9 +9,18 @@ class Day11Test {
 
   @Test
   void partOneExample() {
-    assertEquals(parse("1 2024 1 0 9 9 2021976"), blink(parse("0 1 10 99 999"), 1));
+    assertEquals(parse("1 2024 1 0 9 9 2021976"), breadthFirstBlink(parse("0 1 10 99 999"), 1));
     assertEquals(
         parse("2097446912 14168 4048 2 0 2 4 40 48 2024 40 48 80 96 2 8 6 7 6 0 3 2"),
-        blink(parse("125 17"), 6));
+        breadthFirstBlink(parse("125 17"), 6));
+  }
+
+  @Test
+  void partTwoExample() {
+    assertEquals(
+        parse("1 2024 1 0 9 9 2021976").size(), depthFirstBlink(parse("0 1 10 99 999"), 1));
+    assertEquals(
+        parse("2097446912 14168 4048 2 0 2 4 40 48 2024 40 48 80 96 2 8 6 7 6 0 3 2").size(),
+        depthFirstBlink(parse("125 17"), 6));
   }
 }
