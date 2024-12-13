86084c96804abb80a3c7f13469004cf4893a673d
Marc Löhe <loehe@sipgate.de>
2024-12-13 09:09:54 +0100

    feat: add y2015 d12 p2 (wip)

--- src/main/java/us/byteb/advent/year2015/Day12.java
+++ src/main/java/us/byteb/advent/year2015/Day12.java
@@ -4,26 +4,64 @@ import static us.byteb.advent.Utils.readFileFromResources;
 
 public class Day12 {
 
+  private static final String RED = "\"red\"";
+
   public static void main(final String[] args) {
     final String input = readFileFromResources("year2015/day12.txt");
 
-    System.out.println("Part 1: " + sumAllNumbers(input));
+    // System.out.println("Part 1: " + sumAllNumbers(input, false));
+    System.out.println("Part 2: " + sumAllNumbers(input, true));
   }
 
-  public static long sumAllNumbers(final String input) {
+  public static long sumAllNumbers(final String input, final boolean ignoreRed) {
     long sum = 0L;
     int start = -1;
-    for (int pos = 0; pos < input.length(); pos++) {
+    int pos = 0;
+    outerLoop:
+    while (pos < input.length()) {
       final char c = input.charAt(pos);
+      if (c == '{') {
+        int openBrackets = 1;
+        boolean containsRed = false;
+        for (int endPos = pos + 1; endPos < input.length(); endPos++) {
+          if (openBrackets == 1 && input.startsWith(RED, endPos)) {
+            System.out.println("contains red " + endPos + ", " + openBrackets);
+            containsRed = true;
+          } else if (input.charAt(endPos) == '{') {
+            openBrackets++;
+          } else if (input.charAt(endPos) == '}') {
+            openBrackets--;
+          }
+
+          if (openBrackets == 0) {
+            if (containsRed) System.out.println("contains red " + pos + " - " + endPos);
+            if (!ignoreRed || !containsRed) {
+              final long sub = sumAllNumbers(input.substring(pos + 1, endPos), ignoreRed);
+              System.out.println("+" + sub);
+              sum += sub;
+            }
+            pos = endPos + 1;
+            continue outerLoop;
+          }
+        }
+        throw new IllegalStateException();
+      }
       if (c == '-' || c >= '0' && c <= '9') {
         if (start == -1) start = pos;
       } else {
         if (start != -1) {
-          final long asLong = Long.parseLong(input.substring(start, pos));
-          sum += asLong;
+          final long sub = Long.parseLong(input.substring(start, pos));
+          System.out.println("+" + sub);
+          sum += sub;
           start = -1;
         }
       }
+      pos++;
+    }
+    if (start != -1) {
+      final long sub = Long.parseLong(input.substring(start, pos));
+      System.out.println("+" + sub);
+      sum += sub;
     }
     return sum;
   }
--- src/test/java/us/byteb/advent/year2015/Day12Test.java
+++ src/test/java/us/byteb/advent/year2015/Day12Test.java
@@ -9,7 +9,17 @@ class Day12Test {
 
   @Test
   void partOneExample() {
-    assertEquals(6L, sumAllNumbers("[1,2,3]"));
-    assertEquals(6L, sumAllNumbers("{\"a\":2,\"b\":4}"));
+    assertEquals(6L, sumAllNumbers("[1,2,3]", false));
+    assertEquals(6L, sumAllNumbers("{\"a\":2,\"b\":4}", false));
+  }
+
+  @Test
+  void partTwoExample() {
+    assertEquals(6L, sumAllNumbers("[1,2,3]", true));
+    assertEquals(4L, sumAllNumbers("[1,{\"c\":\"red\",\"b\":2},3]", true));
+    assertEquals(0L, sumAllNumbers("{\"d\":\"red\",\"e\":[1,2,3,4],\"f\":5}", true));
+    assertEquals(6L, sumAllNumbers("[1,\"red\",5]", true));
+    assertEquals(0L, sumAllNumbers("{\"d\":\"red\",\"e\":[1,2,3,4],\"f\":5}", true));
+    assertEquals(6L, sumAllNumbers("[1,\"red\",5]", true));
   }
 }
